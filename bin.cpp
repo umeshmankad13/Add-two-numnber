4a75f6ae8755029db17407bccb9dcc6766301681
umeshmankad13 <umeshmankad555@gmail.com>
2025-02-23 12:14:25 +0530

    Add files via upload

---
+++ bin.cpp
@@ -0,0 +1,81 @@
+//#include<iostream>
+//struct student{
+//	int rno;
+//	float cgpa;
+//	
+//};
+//int main(){
+//	student s[5];
+//	int low ,mid,high ,found=0;
+//	
+//	
+//	std::cout<<"enter the roll no. an cgpa of 5 students \n";
+//	for(int i=0;i<5;i++){
+//		std::cout<<"enter the roll no.and cgpa ["<<(i+1)<<"] ";
+//		std::cin>>s[i].rno>> s[i].cgpa;
+//	}
+//	low=0;
+//	high=4;
+//	std:: cout<<"the student having cgpa is 6.0 ";
+//	while(low<=high){
+//		mid=(low+high)/2;
+//		if(s[mid].cgpa==6.0){
+//			std::cout<<"["<<s[mid].rno<<"]";
+//			found=1;
+//			break;		 
+//		}
+//		else if(s[mid].cgpa>6.0){
+//		high=mid-1;			
+//		}
+//		else{
+//			low =mid+1;
+//		}
+//		if(found==0){
+//			std::cout<<"nothing is equal to 6.0";			
+//		}
+//	}
+//}
+
+
+//#include<iostream>
+//struct student{
+//	int rno;
+//	float cgpa;
+//	
+//};
+//int main(){
+//	student s[5];
+//	int low,mid,high,found=0;
+//		std::cout<<"enter the roll no. and cgpa of 5 student \n";
+//	for(int i=0;i<5;i++){
+//		std::	cout<<"enter the roll no. and cgpa ["<<(i+1)<<"] ";
+//			std::cin>>s[i].rno>>s[i].cgpa;
+//		
+//		
+//		
+//	}
+//	low=0;
+//	high=4;
+//	std::	cout<<"the studnet having cgpa is 6.0";
+//	while(low<=high){
+//		if(s[mid].cgpa==6.0){
+//			std::	cout<<"["<< s[mid].rno <<"]";
+//			found=1;
+//			break;
+//			
+//			
+//		}
+//		else if(s[mid].cgpa>6.0){
+//			high=mid-1;
+//			
+//		}
+//		else{
+//			low=mid+1;
+//			
+//		}
+//		if(found==0){
+//			std::cout<<"nothng equal to 6.0 ";
+//			
+//		}
+//	}
+//}
